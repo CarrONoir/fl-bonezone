2d5b5a0d7344a364ead3144e12df366ae1e7ca37
bone-zone <kovacs.bence1379@gmail.com>
2021-09-28 15:08:07 +0200

    Started bones-from-script
    Made the basics of pulling all the bones from a JS object array. The main reason is for the tooltips but might as well create all from the data. Nicer to edit, and it doesn’t clutter the vanilla HTML as much. Moved the place for the Finishes from the middle to the left column. The idea is that once all slots have been filled, the normal bones disappear, and the mods and finishes show up (left column). Also need to add a remaining slots indicator to the middle... And make that purple! And finally, removed that weird favicon which didn’t work anyway. I’ll mess with that later.

---
+++ bonedata.js
@@ -0,0 +1,25 @@
+const BONEDATA = [
+    {
+        type: "frame",
+        name: "Headless Skeleton",
+        img_link: "images/bonesmall_ph.png",
+        pvalue: 250,
+        skullslots: 1,
+        arms: 2,
+        legs: 2
+    },
+    {
+        type: "frame",
+        name: "Supply a skeleton of your own",
+        img_link: "images/profile_ph.png",
+        pvalue: 250,
+        special: "Licentiates only"
+    },
+    {
+        type: "skull",
+        name: "Cap this with a victim’s skull",
+        img_link: "images/bonesmall_ph.png",
+        pvalue: 250,
+        special: "Licentiates only"
+    }
+]
--- scripts.js
+++ scripts.js
@@ -1,7 +1,27 @@
+window.onload = function() {
+    loadBones(BONEDATA);
+}
+
 function iconBig(x) {
     x.style.transform = "scale(1.1, 1.1)";
 }
 
 function iconNormal(x) {
     x.style.transform = "none";
+}
+
+function createBone(bone_spec) {
+    var bone = document.createElement("li");
+    bone.classList.add("bone");
+    var bone_img = document.createElement("img");
+    bone_img.classList.add("bonepic");
+    bone_img.src = bone_spec.img_link;
+    bone.appendChild(bone_img);
+    document.getElementById(bone_spec.type).appendChild(bone);
+}
+
+function loadBones(bonelist) {
+    for (let i=0;i<bonelist.length;i++) {
+        createBone(bonelist[i]);
+    }
 }
