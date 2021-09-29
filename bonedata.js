37b45dcd21264ded56f1c478fafc63802d042d01
bone-zone <kovacs.bence1379@gmail.com>
2021-09-29 18:02:32 +0200

    Started tooltips
    So far tooltips show up, gathering all the data from the database. (Currently only name and price). They have a fixed width. Not yep optimised for flipping and out-of-screen sliding. Plus added another placeholder for coins. They show up next to the values in the tooltips. Oh, and started extensively documenting the data structure and various scripts. Always useful!

--- bonedata.js
+++ bonedata.js
@@ -1,9 +1,18 @@
+/*Types are: frame,skull,arms,leg,wing,fin,tentacle,tail,mod,finish
+Many things rely on correct type-ing so be careful!
+
+Remember that it's not torso or ribcage but frame! No torsos here!
+Also, yeah, you could call it torso or ribcage instead of frame, but not all of them are torsos or ribcages. All of them Are frames for a skeleton though!
+
+If there's pvalue, there must be pvalue_text as well!
+*/
 const BONEDATA = [
     {
         type: "frame",
         name: "Headless Skeleton",
         img_link: "images/bonesmall_ph.png",
         pvalue: 250,
+        pvalue_text : "250 <img class='bonedesc_img' src='images/coin_ph.png' alt='bonesmall_ph' width='25' height='25'/>", /*Is this cheating? Perhaps.*/
         skullslots: 1,
         arms: 2,
         legs: 2
@@ -13,6 +22,7 @@ const BONEDATA = [
         name: "Supply a skeleton of your own",
         img_link: "images/profile_ph.png",
         pvalue: 250,
+        pvalue_text : "250 <img class='bonedesc_img' src='images/coin_ph.png' alt='bonesmall_ph' width='25' height='25'/>",
         special: "Licentiates only"
     },
     {
@@ -20,6 +30,7 @@ const BONEDATA = [
         name: "Cap this with a victim’s skull",
         img_link: "images/bonesmall_ph.png",
         pvalue: 250,
+        pvalue_text : "250 <img class='bonedesc_img' src='images/coin_ph.png' alt='bonesmall_ph' width='25' height='25'/>",
         special: "Licentiates only"
     }
 ]
--- scripts.js
+++ scripts.js
@@ -1,27 +1,63 @@
 window.onload = function() {
-    loadBones(BONEDATA);
+    loadBones(BONEDATA); /*Fills up the left section*/
 }
 
-function iconBig(x) {
-    x.style.transform = "scale(1.1, 1.1)";
+function iconBig(element) {
+    element.style.transform = "scale(1.1, 1.1)";
 }
 
-function iconNormal(x) {
-    x.style.transform = "none";
+function iconNormal(element) {
+    element.style.transform = "none";
 }
 
-function createBone(bone_spec) {
+function createBone(bone_spec) { /*Creates a bone-icon with all of it's functions for use in the left column. bone_spec is a single element from the data array*/
     var bone = document.createElement("li");
     bone.classList.add("bone");
     var bone_img = document.createElement("img");
     bone_img.classList.add("bonepic");
+    bone_img.style.cursor = "pointer";
+
+    //bone_img.onmouseover = function(){iconBig(this)};
+    bone_img.onmouseover = function(){createTT(this.parentElement, bone_spec)};
+    //bone_img.onmouseout = function(){iconNormal(this)};
+    bone_img.onmouseout = function(){deleteTT()};
+
     bone_img.src = bone_spec.img_link;
+
     bone.appendChild(bone_img);
     document.getElementById(bone_spec.type).appendChild(bone);
 }
 
-function loadBones(bonelist) {
+function loadBones(bonelist) { /*Creates all the bones from the bonelist array*/
     for (let i=0;i<bonelist.length;i++) {
         createBone(bonelist[i]);
+    }
+}
+
+function createTT(element, bone) { /*Creates a tooltip of a specific bone, and assigns it under element*/
+    var ttcont = document.createElement("div"); /*Main wrapper*/
+    ttcont.classList.add("ttcont");
+
+    var bname = document.createElement("h3"); /*Name*/
+    bname.innerHTML = bone.name;
+    ttcont.appendChild(bname);
+
+    if (bone.pvalue) { /*Show price-text, if there is a price. Only the mods and finishes don't have pvalues, but they are stored in the same database, due to appearing in the same way*/
+        var pvalue = document.createElement("span");
+        pvalue.innerHTML = bone.pvalue_text;
+        ttcont.appendChild(pvalue);
+    }
+    
+    var spacer0 = document.createElement("hr"); /*Constant hr. This is always here. There is always something after the price.*/
+    spacer0.classList.add("desc");
+    ttcont.appendChild(spacer0);
+
+    element.appendChild(ttcont);
+}
+
+function deleteTT() {    /*Deletes all currently active tooltips. There shouldn't be more than 1 at any time, but just to be safe*/
+    var tt = document.getElementsByClassName("ttcont");
+    for (let i=0;i<tt.length;i++) {
+        tt[i].remove();
     }
 }
