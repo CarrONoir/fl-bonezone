554852f943f9d704bc28b2486b65992ecf0d643c
bone-zone <kovacs.bence1379@gmail.com>
2021-09-29 22:52:13 +0200

    Tooltips for frames and bones
    Tooltips should work for all the attributes of frames and bones. Mods and finishes remain for another day. Much of the data structure has been added, and the tooltip ordering extended. Extensively documented!

--- bonedata.js
+++ bonedata.js
@@ -1,36 +1,93 @@
-/*Types are: frame,skull,arms,leg,wing,fin,tentacle,tail,mod,finish
-Many things rely on correct type-ing so be careful!
+/*Array or objects for the data of the various bones. Automate everything!
 
 Remember that it's not torso or ribcage but frame! No torsos here!
 Also, yeah, you could call it torso or ribcage instead of frame, but not all of them are torsos or ribcages. All of them Are frames for a skeleton though!
 
-If there's pvalue, there must be pvalue_text as well!
+TAG LIST (only include the ones you use):
+type - element type (frame,skull,arm,leg,wing,fin,tentacle,tail,mod,finish) Many things rely on the types so be careful!
+tstyle - only for frame types, in-game number
+name - element name (MUST HAVE)
+img_link - full path pls
+pvalue - Penny value
+sourceTag - Text to signify source (eg. Fate, Ambition, Seasonal etc.)
+skullslots, limbslots, tailslots - Slots (used only for frame types)
+bskulls, barms, blegs, bwings, bfins, btentacles, btails - Built-in parts (used only for frame types)
+ch_lv - Challenge level (base)
+ch_nm - Challenge name
+ch_ex - Extra text for challenge
+antiquity, amalgamy, menace, supportcc, implausibility - Attributes (max)
+antiquity_dif, amalgamy_dif, menace_dif, supportcc_dif, implausibility_dif - Attributes (min-difference), when there are challenges, higher minus the lower ('cuz a zero would be false in logistics)
+special - Additional text (always displayed last)
+
+Basic order (optional, but more organised if followed):
+For Frames: Type, Torso Style, Name, Image, Value, SourceTag, Slots, Built-in parts, Attributes, Special text
+For Bones (skulls, limbs and tails): Type, Name, Image, Value, SourceTag, Challange, Challange extra text, Attributes, Special text
+For Mods:
+For Finishes:
+
+sourceTag: "<span class='fate'>FATE</span>",
 */
 const BONEDATA = [
     {
         type: "frame",
+        tstyle: 10,
         name: "Headless Skeleton",
         img_link: "images/bonesmall_ph.png",
         pvalue: 250,
-        pvalue_text : "250 <img class='bonedesc_img' src='images/coin_ph.png' alt='bonesmall_ph' width='25' height='25'/>", /*Is this cheating? Perhaps.*/
         skullslots: 1,
-        arms: 2,
-        legs: 2
+        barms: 2,
+        blegs: 2
     },
     {
         type: "frame",
+        tstyle: 10,
         name: "Supply a skeleton of your own",
-        img_link: "images/profile_ph.png",
+        img_link: "images/bonesmall_ph.png",
         pvalue: 250,
-        pvalue_text : "250 <img class='bonedesc_img' src='images/coin_ph.png' alt='bonesmall_ph' width='25' height='25'/>",
-        special: "Licentiates only"
+        skullslots: 1,
+        barms: 2,
+        blegs: 2,
+        special: "<span class='rust'>Licentiates</span> only (free)"
+    },
+    {
+        type: "frame",
+        tstyle: 15,
+        name: "Human Ribcage",
+        img_link: "images/bonesmall_ph.png",
+        pvalue: 1250,
+        skullslots: 1,
+        limbslots: 4,
+    },
+    {
+        type: "frame",
+        tstyle: 80,
+        name: "Prismatic Frame",
+        img_link: "images/bonesmall_ph.png",
+        pvalue: 31250,
+        skullslots: 3,
+        limbslots: 3,
+        tailslots: 3,
+        antiquity: 2,
+        amalgamy: 2,
+        special: "Requires Your Own Stall 3"
     },
     {
         type: "skull",
         name: "Cap this with a victim’s skull",
         img_link: "images/bonesmall_ph.png",
         pvalue: 250,
-        pvalue_text : "250 <img class='bonedesc_img' src='images/coin_ph.png' alt='bonesmall_ph' width='25' height='25'/>",
-        special: "Licentiates only"
+        special: "<span class='rust'>Licentiates</span> only (free)"
+    },
+    {
+        type: "skull",
+        name: "Carved Ball of Stygian Ivory",
+        img_link: "images/bonesmall_ph.png",
+        pvalue: 250,
+        ch_lv: 6,
+        ch_nm: "<span class='green'>Mithridacy</span>",
+        ch_ex: "Reduced by 1 per Skull installed",
+        implausibility: 2,
+        implausibility_dif: 2,
+        special: "Reduces <span class='pink'>Skullslots</span>, but doesn't count as a Skull"
     }
 ]
--- scripts.js
+++ scripts.js
@@ -17,10 +17,8 @@ function createBone(bone_spec) { /*Creates a bone-icon with all of it's function
     bone_img.classList.add("bonepic");
     bone_img.style.cursor = "pointer";
 
-    //bone_img.onmouseover = function(){iconBig(this)};
-    bone_img.onmouseover = function(){createTT(this.parentElement, bone_spec)};
-    //bone_img.onmouseout = function(){iconNormal(this)};
-    bone_img.onmouseout = function(){deleteTT()};
+    bone_img.onmouseover = function(){iconBig(this);createTT(this.parentElement, bone_spec)};
+    bone_img.onmouseout = function(){iconNormal(this);deleteTT()};
 
     bone_img.src = bone_spec.img_link;
 
@@ -35,22 +33,182 @@ function loadBones(bonelist) { /*Creates all the bones from the bonelist array*/
 }
 
 function createTT(element, bone) { /*Creates a tooltip of a specific bone, and assigns it under element*/
-    var ttcont = document.createElement("div"); /*Main wrapper*/
+    /*This must cycle through and check all the possible properties a bone could have, comparing it to the data array
+    The order here is the order on the tooltips. Chech for later ones to determing hr(|). Following this:
+    For Frames: Name, Value, SourceTag |, Style |, Slots |, Built-in parts |, Attributes |, Special text
+    For Bones (skulls, limbs and tails): Name, Value, SourceTag |, Challange, Challange extra text |, Attributes |, Special text
+    For Mods:
+    For Finishes:
+    (was used when building the order)
+
+    Things labaled as "Constant" in their comment are always appearing elements that need no checking.
+    */
+
+    var ttcont = document.createElement("div"); /*Constant Main wrapper*/
     ttcont.classList.add("ttcont");
 
-    var bname = document.createElement("h3"); /*Name*/
+    var bname = document.createElement("h3"); /*Constant Name*/
     bname.innerHTML = bone.name;
     ttcont.appendChild(bname);
 
-    if (bone.pvalue) { /*Show price-text, if there is a price. Only the mods and finishes don't have pvalues, but they are stored in the same database, due to appearing in the same way*/
+    if (bone.pvalue) { /*Price-and-text. (mods and finishes don't have prices)*/
         var pvalue = document.createElement("span");
-        pvalue.innerHTML = bone.pvalue_text;
+        pvalue.innerHTML = bone.pvalue + " <img class='bonedesc_img' src='images/coin_ph.png' alt='bonesmall_ph' width='25' height='25'/>";
         ttcont.appendChild(pvalue);
     }
+
+    if (bone.sourceTag) { /*Source indicator (eg. Fate, Ambition etc.)*/
+        var sourceTag = document.createElement("span");
+        sourceTag.innerHTML = bone.sourceTag;
+        ttcont.appendChild(sourceTag);
+    }
     
-    var spacer0 = document.createElement("hr"); /*Constant hr. This is always here. There is always something after the price.*/
+    var spacer0 = document.createElement("hr"); /*Constant hr. There is always something after the price.*/
     spacer0.classList.add("desc");
     ttcont.appendChild(spacer0);
+
+    if (bone.tstyle) { /*Torso (frame) style (While frames are special, it can't just check for frames, due to ordering sheninigans. (Built in parts come after the attributes.))*/
+        var tstyle = document.createElement("span");
+        tstyle.innerHTML = "Torso Style: " + bone.tstyle;
+        ttcont.appendChild(tstyle);
+        var spacer_ts = document.createElement("hr");
+        spacer_ts.classList.add("desc");
+        ttcont.appendChild(spacer_ts);
+    }
+
+    if (bone.skullslots || bone.limbslots || bone.tailslots) { /*The open slots on frames*/
+        if (bone.skullslots) {
+            var skullslots = document.createElement("span");
+            skullslots.innerHTML = bone.skullslots + " x <span class='pink'>Skullslots</span>";
+            ttcont.appendChild(skullslots);
+        }
+        if (bone.limbslots) {
+            var limbslots = document.createElement("span");
+            limbslots.innerHTML = bone.limbslots + " x <span class='pink'>Limbslots</span>";
+            ttcont.appendChild(limbslots);
+        }
+        if (bone.tailslots) {
+            var tailslots = document.createElement("span");
+            tailslots.innerHTML = bone.tailslots + " x <span class='pink'>Tailslots</span>";
+            ttcont.appendChild(tailslots);
+        }
+        if (bone.bskulls || bone.barms || bone.blegs || bone.bwings || bone.bfins || bone.btentacles || bone.btails || bone.antiquity || bone.menace || bone.amalgamy || bone.supportcc || bone.implausibility || bone.special) { /*Is there more?*/
+            var spacer_slots = document.createElement("hr");
+            spacer_slots.classList.add("desc");
+            ttcont.appendChild(spacer_slots);
+        }
+    }
+
+    if (bone.ch_lv && bone.ch_nm) { /*Challanges. Frames and bone attribues (like slots) don't mix with a precise database.*/
+        var challenge = document.createElement("span");
+        challenge.innerHTML = bone.ch_lv + " " + bone.ch_nm;
+        ttcont.appendChild(challenge);
+        if (bone.ch_ex) {
+            var challenge_ex = document.createElement("span");
+            challenge_ex.innerHTML = bone.ch_ex;
+            ttcont.appendChild(challenge_ex);
+        }
+        if (bone.antiquity || bone.menace || bone.amalgamy || bone.supportcc || bone.implausibility || bone.special) {
+            var spacer_chall = document.createElement("hr");
+            spacer_chall.classList.add("desc");
+            ttcont.appendChild(spacer_chall);
+        }
+    }
+
+    if (bone.bskulls || bone.barms || bone.blegs || bone.bwings || bone.bfins || bone.btentacles || bone.btails) { /*For frames that come with built-in parts*/
+        var builtin_notice = document.createElement("span");
+        builtin_notice.innerHTML = "Built-in:";
+        ttcont.appendChild(builtin_notice);
+        if (bone.bskulls) {
+            var bskulls = document.createElement("span");
+            bskulls.innerHTML = bone.bskulls + " x Skulls";
+            ttcont.appendChild(bskulls);
+        }
+        if (bone.barms) {
+            var barms = document.createElement("span");
+            barms.innerHTML = bone.barms + " x Arms";
+            ttcont.appendChild(barms);
+        }
+        if (bone.blegs) {
+            var blegs = document.createElement("span");
+            blegs.innerHTML = bone.blegs + " x Legs";
+            ttcont.appendChild(blegs);
+        }
+        if (bone.bwings) {
+            var bwings = document.createElement("span");
+            bwings.innerHTML = bone.bwings + " x Wings";
+            ttcont.appendChild(bwings);
+        }
+        if (bone.bfins) {
+            var bfins = document.createElement("span");
+            bfins.innerHTML = bone.bfins + " x Fins";
+            ttcont.appendChild(bfins);
+        }
+        if (bone.btentacles) {
+            var btentacles = document.createElement("span");
+            btentacles.innerHTML = bone.btentacles + " x Tentacles";
+            ttcont.appendChild(btentacles);
+        }
+        if (bone.btails) {
+            var btails = document.createElement("span");
+            btails.innerHTML = bone.btails + " x Tails";
+            ttcont.appendChild(btails);
+        }
+        if (bone.antiquity || bone.menace || bone.amalgamy || bone.supportcc || bone.implausibility || bone.special) { /*Is there more?*/
+            var spacer_builtin = document.createElement("hr");
+            spacer_builtin.classList.add("desc");
+            ttcont.appendChild(spacer_builtin);
+        }
+    }
+
+    if (bone.antiquity || bone.menace || bone.amalgamy || bone.supportcc || bone.implausibility) { /*Attributes*/
+        if (bone.antiquity) {
+            var antiquity = document.createElement("span");
+            if (bone.antiquity_min) antiquity.innerHTML = bone.antiquity_min + "-" + bone.antiquity + " x <span class='yellow'>Antiquity</span>";
+            else antiquity.innerHTML = bone.antiquity + " x <span class='yellow'>Antiquity</span>";
+            ttcont.appendChild(antiquity);
+        }
+        if (bone.menace) {
+            var menace = document.createElement("span");
+            if (bone.menace_min) menace.innerHTML = bone.menace_min + "-" + bone.menace + " x <span class='red'>Menace</span>";
+            else menace.innerHTML = bone.menace + " x <span class='red'>Menace</span>";
+            ttcont.appendChild(menace);
+        }
+        if (bone.amalgamy) {
+            var amalgamy = document.createElement("span");
+            amalgamy.innerHTML = bone.amalgamy + " x <span class='orange'>Amalgamy</span>";
+            ttcont.appendChild(amalgamy);
+        }
+        if (bone.supportcc) {
+            var supportcc = document.createElement("span");
+            supportcc.innerHTML = bone.supportcc + " x <span class='ccblue'>CC Support</span>";
+            ttcont.appendChild(supportcc);
+        }
+        if (bone.implausibility) {
+            var implausibility = document.createElement("span");
+            if (bone.implausibility_dif) { //Oh yeah 0 is false..
+                implausibility.innerHTML = (bone.implausibility-bone.implausibility_dif) + "-" + bone.implausibility + " x <span class='grey'>Implausibility</span>";
+            } else {
+                implausibility.innerHTML = bone.implausibility + " x <span class='grey'>Implausibility</span>";
+            }
+            ttcont.appendChild(implausibility);
+        }
+        if (bone.special) {
+            var spacer_attributes = document.createElement("hr");
+            spacer_attributes.classList.add("desc");
+            ttcont.appendChild(spacer_attributes);
+        }
+    }
+
+    if (bone.special) { /*Extra text*/
+        var special = document.createElement("span");
+        special.innerHTML = bone.special;
+        ttcont.appendChild(special);
+    }
+
+    var spacer_end = document.createElement("hr");  /*Adds a little space to the end*/
+    spacer_end.classList.add("end");
+    ttcont.appendChild(spacer_end);
 
     element.appendChild(ttcont);
 }
