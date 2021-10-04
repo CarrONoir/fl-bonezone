858cf3bbfcf42f5b9d55728842307dede974230a
bone-zone <kovacs.bence1379@gmail.com>
2021-10-04 22:58:14 +0200

    Started main calculation
    Values and functions for the main calculations started. Recognises frames and limbs so far. State machine basics with 3 states (needs a review). Added display for free slots to the middle. Note: make sure the attributes can’t go below 0.

--- scripts.js
+++ scripts.js
@@ -2,6 +2,18 @@ window.onload = function() {
     loadBones(BONEDATA); //Fills up the left section
 }
 
+let pvalue = 0;
+let amalgamy = 0;let amalgamy_dif = 0;
+let antiquity = 0;let antiquity_dif = 0;
+let menace = 0;let menace_dif = 0;
+let implausibility = 0;let implausibility_dif = 0;
+let supportcc = 0;let supportcc_dif = 0;
+let skullslots = 0;let limbslots = 0;let tailslots = 0;
+let skulls = 0;let arms = 0;let legs = 0;let wings = 0;let fins = 0;let tentacles = 0;let tails = 0;
+let tstyle = 0;
+let listlenght = 0;
+let state_comp = 0; //0-frame 1-bones 2-mods
+
 //Creates all the bones from the bonelist array
 function loadBones(bonelist) {
     for (let i=0;i<bonelist.length;i++) {
@@ -27,6 +39,7 @@ function createBone(bone_spec) {
 
     bone_img.onmouseover = function(){iconBig(this);createTT(this.parentElement, bone_spec)};
     bone_img.onmouseout = function(){iconNormal(this);deleteTT()};
+    bone_img.onclick = function(){addBone(bone_spec)};
 
     bone_img.src = bone_spec.img_link;
 
@@ -170,9 +183,132 @@ function createTT(element, bone) {
     element.appendChild(ttcont);
 }
 
-function deleteTT() {    /*Deletes all currently active tooltips. There shouldn't be more than 1 at any time, but just to be safe*/
+/*Deletes all currently active tooltips. There shouldn't be more than 1 at any time, but just to be safe*/
+function deleteTT() {
     var tt = document.getElementsByClassName("ttcont");
     for (let i=0;i<tt.length;i++) {
         tt[i].remove();
     }
+}
+
+/*Adds selected bone to the current calculation.*/
+function addBone(bone) {
+    switch (state_comp) {
+        case 0:
+            if (bone.type == "frame") {
+                document.getElementById("d_framename").innerHTML = bone.name;//Shut up I know, this is an exception!
+                if (bone.skullslots) skullslots = bone.skullslots;
+                if (bone.limbslots) limbslots = bone.limbslots;
+                if (bone.tailslots) tailslots = bone.tailslots;
+                if (bone.tstyle) tstyle = bone.tstyle;
+                if (bone.bskulls) skulls = bone.bskulls;
+                if (bone.barms) arms = bone.barms;
+                if (bone.blegs) legs = bone.blegs;
+                if (bone.bwings) wings = bone.bwings;
+                if (bone.bfins) fins = bone.bfins;
+                if (bone.btentacles) tentacles = bone.btentacles;
+                if (bone.btails) tails = bone.btails;
+                if (bone.amalgamy) amalgamy = bone.amalgamy;
+                if (bone.amalgamy_dif) amalgamy_dif = bone.amalgamy_dif;
+                if (bone.antiquity) antiquity = bone.antiquity;
+                if (bone.antiquity_dif) antiquity_dif = bone.antiquity_dif;
+                if (bone.menace) menace = bone.menace;
+                if (bone.menace_dif) menace_dif = bone.menace_dif;
+                if (bone.supportcc) supportcc = bone.supportcc;
+                if (bone.supportcc_dif) supportcc_dif = bone.supportcc_dif;
+                if (bone.implausibility) implausibility = bone.implausibility;
+                if (bone.implausibility_dif) implausibility_dif = bone.implausibility_dif;
+                pvalue += bone.pvalue;
+                addLine(bone);
+                state_comp = 1;
+            }
+            break;
+        case 1:
+            var changed = false;
+            if (skullslots > 0 && bone.type == "skull") {
+                skulls++;
+                skullslots--;
+                changed = true;
+            }
+            if (limbslots > 0 && (bone.type == "arm" || bone.type == "leg" || bone.type == "wing" || bone.type == "fin" || bone.type == "tentacle")) {
+                switch (bone.type) {
+                    case "arm":
+                        arms++;
+                        break;
+                    case "leg":
+                        legs++;
+                        break;
+                    case "wing":
+                        wings++;
+                        break;
+                    case "fin":
+                        fins++;
+                        break;
+                    case "tentacle":
+                        tentacles++;
+                        break;
+                }
+                limbslots--;
+                changed = true;
+            }
+            if (tailslots > 0 && bone.type == "tail") {
+                tails++;
+                tailslots--;
+                changed = true;
+            }
+            if (changed) {
+                if (bone.amalgamy) amalgamy += bone.amalgamy;
+                if (bone.amalgamy_dif) amalgamy_dif += bone.amalgamy_dif;
+                if (bone.antiquity) antiquity += bone.antiquity;
+                if (bone.antiquity_dif) antiquity_dif += bone.antiquity_dif;
+                if (bone.menace) menace += bone.menace;
+                if (bone.menace_dif) menace_dif += bone.menace_dif;
+                if (bone.supportcc) supportcc += bone.supportcc;
+                if (bone.supportcc_dif) supportcc_dif += bone.supportcc_dif;
+                if (bone.implausibility) implausibility += bone.implausibility;
+                if (bone.implausibility_dif) implausibility_dif += bone.implausibility_dif;
+                pvalue += bone.pvalue;
+                addLine(bone);
+            }
+            break;
+        case 2:
+            break;
+    }
+
+    updateDisplay();
+}
+
+/*Updates the display accoding to the numbers in the inner calculation.*/
+function updateDisplay() {
+    document.getElementById("d_pvalue").innerHTML = pvalue;
+    document.getElementById("d_skulls").innerHTML = skulls;
+    document.getElementById("d_arms").innerHTML = arms;
+    document.getElementById("d_legs").innerHTML = legs;
+    document.getElementById("d_wings").innerHTML = wings;
+    document.getElementById("d_fins").innerHTML = fins;
+    document.getElementById("d_tentacles").innerHTML = tentacles;
+    document.getElementById("d_tails").innerHTML = tails;
+
+    document.getElementById("d_skullslots").innerHTML = skullslots;
+    document.getElementById("d_limbslots").innerHTML = limbslots;
+    document.getElementById("d_tailslots").innerHTML = tailslots;
+
+    if (amalgamy_dif) document.getElementById("d_amalgamy").innerHTML = (amalgamy-amalgamy_dif) + "-" + amalgamy;
+    else document.getElementById("d_amalgamy").innerHTML = amalgamy;
+    if (antiquity_dif) document.getElementById("d_antiquity").innerHTML = (antiquity-antiquity_dif) + "-" + antiquity;
+    else document.getElementById("d_antiquity").innerHTML = antiquity;
+    if (menace_dif) document.getElementById("d_menace").innerHTML = (menace-menace_dif) + "-" + menace;
+    else document.getElementById("d_menace").innerHTML = menace;
+    if (supportcc_dif) document.getElementById("d_supportcc").innerHTML = (supportcc-supportcc_dif) + "-" + supportcc;
+    else document.getElementById("d_supportcc").innerHTML = supportcc;
+    if (implausibility_dif) document.getElementById("d_implausibility").innerHTML = (implausibility-implausibility_dif) + "-" + implausibility;
+    else document.getElementById("d_implausibility").innerHTML = implausibility;
+}
+
+/*Adds a line to the bottom of the middle column, with info about the specific bone */
+function addLine(bone) {
+    var line = document.createElement("div");
+    line.innerHTML = (listlenght+1) + ". " + bone.name;
+    document.getElementById("listorder").appendChild(line);
+    listlenght++;
 }
